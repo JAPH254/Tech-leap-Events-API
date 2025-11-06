322fcae4e6fc68374788549b7e0a7b0bc2f3dd3a
JAPH254 <omanyujapheth@gmail.com>
2025-11-06 14:17:00 +0300

    feat: added the models

--- events/models.py
+++ events/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from auth_users.models import CustomUser
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateTimeField()
+    location = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    approved = models.BooleanField(default=False)
+    isOrganizer = models.BooleanField(default=False)
+    isAttendee = models.BooleanField(default=True)
+    RSVP_count = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
