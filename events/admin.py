ffe7712ba0960c0eb6a46ed18b969dcb1e7f8661
JAPH254 <omanyujapheth@gmail.com>
2025-11-06 14:16:43 +0300

    feat: added the admin configuration

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import Event
 # Register your models here.
+admin.site.register(Event, list_display=['title', 'date', 'location', 'is_active', 'approved'], list_filter=['is_active', 'approved', 'date'])
