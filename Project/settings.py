e13a1f4a967f9c7c6af926e6400400b84a50c87f
JAPH254 <omanyujapheth@gmail.com>
2025-11-06 13:54:47 +0300

    feat: add REST framework configuration and custom user model

--- Project/settings.py
+++ Project/settings.py
@@ -41,6 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
+    'auth_users',
+    'events',
+
 ]
 
 MIDDLEWARE = [
@@ -137,3 +143,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+}
+
+AUTH_USER_MODEL = 'auth_users.CustomUser'
