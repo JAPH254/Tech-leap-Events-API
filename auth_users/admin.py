6d860a84d7513b271b754193872c3749cb57fcb8
JAPH254 <omanyujapheth@gmail.com>
2025-11-06 14:03:44 +0300

    feat: register CustomUser model in admin with display and filter options

--- auth_users/admin.py
+++ auth_users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CustomUser
+
+@admin.register(CustomUser)
+class AuthUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'is_active', 'date_joined')
+    search_fields = ('username', 'email')
+    list_filter = ('is_active', 'date_joined')
